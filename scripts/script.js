3a2e37b995314e6028e3bc068ed396d18e0cd677
Altunyan <altuyanash@mail.ru>
2023-09-02 21:03:32 +0400

    projectAsh

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+// // console.log(true);
+// // let a = 12;
+// // a = 78;
+// let a: number=78;
+// let b:string='hello';
+// let c:boolean=true;
+// let d:null=null;
+// let e:undefined=undefined;
+// function test(params:number,s?:string):number {
+//     return 77;
+// }
+// test(45);
+// function v(params:type):void {    
+// }
+// let data:any =45;
+// let date:unknown = 45;
+// console.log(data +5);
+// let arr:[number,string,boolean]=[12,'hello',true];
+// let user = {
+//     name:'mike',
+//     last_name:'smith',
+//     email:'test@mail.ru',
+//     phone_nomber:'+374999999',
+//     age:12,
+//     child:{
+//         name:'make',
+//         last_name:'smith',
+//         age:12,
+//     }
+// };
+// class User{
+//     name:string;
+//     last_name:string;
+//     email:string|number;
+//     phone_nomber:number;
+//     age:number;
+//     child:(name:string)
+// }
+// function test<T>(params:T) {
+// }
+// test<number>(78);
+// test<boolean>(true);
+// test<{name:string}>({name:'mike'});
+var btn = document.getElementsByClassName("select");
+var firstWords = "hello world";
+console.log(firstWords);
